cf35fc00f647b02f9945cfc88c5856e5705adea2
Ronan-Lef <ronan.lefour@edu.univ-fcomte.fr>
2024-05-27 09:32:05 +0200

    TP5

---
+++ src/components/AgentCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Collections, type AgentsResponse } from '@/pocketbase-types';
+const props = defineProps<AgentsResponse>()
+</script>
+<template>
+    <article class="border border-teal-500 rounded-lg w-fit p-4 m-2">
+        <div class="flex gap-1">
+            <h3>{{ prenom }}</h3>
+            <p>{{ nom }}</p>
+        </div>
+        <div>
+            <p>{{ telephone }}</p>
+            <p>{{ email }}</p>
+        </div>
+    </article>
+</template>
--- src/pages/agents/[id].vue
+++ src/pages/agents/[id].vue
@@ -1,12 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-  import { pb } from '@/backend';
+import { pb } from '@/backend'
 import { useRoute } from 'vue-router/auto'
+import AgentCard from '@/components/AgentCard.vue'
 
-  //import { oneIDagent } from '@/backend'
-
-  const route = useRoute('/agents/[id]')
-  console.log('id :', route.params.id)
+const route = useRoute('/agents/[id]')
+console.log('id :', route.params.id)
 
 const unAgent = await pb.collection('Agents').getOne(route.params.id)
 </script>
@@ -14,7 +13,9 @@ const unAgent = await pb.collection('Agents').getOne(route.params.id)
 <template>
   <div>
     <h1 class="text-xl">Un Agent</h1>
+    <AgentCard v-if="unAgent" v-bind="unAgent" />
     <p>Nom agent : {{ unAgent.nomAgent }}</p>
-    <p>email : {{ unAgent.emailAgent }}</p>
+    <p>Email : {{ unAgent.emailAgent }}</p>
+    <p>Téléphone : {{ unAgent.telephoneAgent }}</p>
   </div>
 </template>
